099581882319f49b4055ff07f1d4b569f439f730
Matt <mattdholmes@comcast.net>
2019-12-11 17:57:18 -0500

    Pre UI

---
+++ Assets/Scripts/Group.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Group : MonoBehaviour
+{
+    private float previousTime;
+    public float fallTime = 0.8f;
+
+    bool isValidGridPos()
+    {
+        foreach (Transform child in transform)
+        {
+            Vector2 v = Playfield.roundVec2(child.position);
+            if (!Playfield.insideBorder(v))
+                return false;
+            if (Playfield.grid[(int)v.x, (int)v.y] != null && Playfield.grid[(int)v.x, (int)v.y].parent != transform)
+                return false;
+
+        }
+        return true;
+    }
+
+    void updateGrid()
+    {
+        for (int y = 0; y < Playfield.h; ++y)
+            for (int x = 0; x < Playfield.w; ++x)
+                if (Playfield.grid[x, y] != null)
+                    if (Playfield.grid[x, y].parent == transform)
+                        Playfield.grid[x, y] = null;
+
+        foreach (Transform child in transform)
+        {
+            Vector2 v = Playfield.roundVec2(child.position);
+            Playfield.grid[(int)v.x, (int)v.y] = child;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!isValidGridPos())
+        {
+            Debug.Log("GAME OVER");
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.position += new Vector3(-1, 0, 0);
+            if (isValidGridPos())
+                updateGrid();
+            else
+                transform.position += new Vector3(1, 0, 0);
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.position += new Vector3(1, 0, 0);
+            if (isValidGridPos())
+                updateGrid();
+            else
+                transform.position += new Vector3(-1, 0, 0);
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            transform.Rotate(0, 0, -90);
+            if (isValidGridPos())
+                updateGrid();
+            else
+                transform.Rotate(0, 0, 90);
+        }
+        else if (Input.GetKey(KeyCode.DownArrow) || Time.time - previousTime >= 1)
+        {
+            transform.position += new Vector3(0, -1, 0);
+            if (isValidGridPos())
+            {
+                updateGrid();
+            }
+            else
+            {
+                transform.position += new Vector3(0, 1, 0);
+                Playfield.deleteFullRows();
+                FindObjectOfType<Spawner>().spawnNext();
+
+                enabled = false;
+            }
+            previousTime = Time.time;
+        }
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject[] groups;
+
+    public void spawnNext()
+    {
+        Instantiate(groups[Random.Range(0, groups.Length)], transform.position, Quaternion.identity);
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnNext();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
